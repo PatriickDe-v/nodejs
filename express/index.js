fb237599d240d478f1aaf2512c63d9e2d802cf9a
Patrick <patriick.dev@hotmail.com>
2025-06-24 12:40:26 -0300

    nodemon and parametros

--- express/index.js
+++ express/index.js
@@ -1,16 +1,33 @@
 const express = require("express");     //Importando o Express
 const app = express();                   //Iniciando o express e passando inicialização para const app
 
+// REQ => DADOS ENVIADOS PELO USUÁRIO
+// RES => RESPOSTA QUE VAI SER ENVIADA PELO USUÁRIO
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.send("Bem vindo ao meu site!")      //Definindo rota inicial apenas uma resposta por rota
-})                            
-
-app.get("/blog", function(req, res) {       //Segunda rota criada
-    res.send("Olá, você está no blog")
+})
+//Parametro opcional
+app.get("/blog/:artigo?", function (req, res) {       //Segunda rota criada
+    
+    let artigo = req.params.artigo
+    if (artigo) {
+        res.send(`Você está no artigo: ${artigo}`)
+    } else {
+        res.send("Você está no blog")
+    }
 })
 
-app.listen(8080, function (erro) {              //Adicionando a porta e a função caso der algum erro, mostrar mensagem
+
+app.get("/ola/:nome", function (req, res) {
+    let nome = req.params.nome
+    res.send(`Olá! ${nome}`)
+})
+
+
+
+
+app.listen(4000, function (erro) {              //Adicionando a porta e a função caso der algum erro, mostrar mensagem
     if (erro) {
         console.log("Ocorrou um erro!")
     } else {
