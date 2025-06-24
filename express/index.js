8f17b694ca3a066b70673f93d0aca2e8405a512e
Patrick <patriick.dev@hotmail.com>
2025-06-24 11:37:40 -0300

    iniciando em rotas com express

--- express/index.js
+++ express/index.js
@@ -1,6 +1,15 @@
 const express = require("express");     //Importando o Express
 const app = express();                   //Iniciando o express e passando inicialização para const app
 
+
+app.get("/", function(req, res) {
+    res.send("Bem vindo ao meu site!")      //Definindo rota inicial apenas uma resposta por rota
+})                            
+
+app.get("/blog", function(req, res) {       //Segunda rota criada
+    res.send("Olá, você está no blog")
+})
+
 app.listen(8080, function (erro) {              //Adicionando a porta e a função caso der algum erro, mostrar mensagem
     if (erro) {
         console.log("Ocorrou um erro!")
